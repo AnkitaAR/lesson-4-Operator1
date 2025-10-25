54af8327d1dc6adcbab1ea1005a897f3ce6900d9
AnkitaAR <jainankita659@gmail.com>
2025-10-25 16:13:16 +0530

    first commit

---
+++ average.py
@@ -0,0 +1,15 @@
+#storing values
+tree1=98;
+tree2=94;
+tree3=41;
+tree4=95;
+tree5=11;
+
+# adding values
+sum=tree1+tree2+tree3+tree4+tree5;
+
+#caluclating the average
+avg=sum/5
+
+#print the output
+print("The average height is:",avg)

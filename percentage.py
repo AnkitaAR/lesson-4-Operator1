3b78152f41b7227280be04cbf822634b24497a36
AnkitaAR <67515597+AnkitaAR@users.noreply.github.com>
2025-10-27 16:56:51 +0530

    Add files via upload

---
+++ percentage.py
@@ -0,0 +1,8 @@
+print("Enter the marks of 4 subjects:")
+eng=int(input("English:"))
+maths=int(input("Maths:"))
+science=int(input("Science:"))
+social_studies=int(input("Social Studies:"))
+sum=eng+maths+science+social_studies
+perc=(sum/400)*100
+print("Percentage:",perc)
